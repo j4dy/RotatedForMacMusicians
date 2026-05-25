1e603de01a6d1c1351330d4e390b1bb00b86019f
Judy Wong <judy@j4dy.net>
2026-05-25 20:06:36 +0800

    Removed all test source files and binaries, keeping only actual compilation files

--- test_mouse_warp.swift
+++
@@ -1,9 +0,0 @@
-import AppKit
-
-func testWarp() {
-    print("Testing cursor warp")
-    let point = CGPoint(x: 100, y: 100)
-    CGWarpMouseCursorPosition(point)
-    print("Warped to \(point)")
-}
-testWarp()
--- test_scroll_fields.swift
+++
@@ -1,6 +0,0 @@
-import AppKit
-print("CGEvent fields test")
-let cgEvent = CGEvent(scrollWheelEvent2Source: nil, units: .pixel, wheelCount: 2, wheel1: 10, wheel2: 5, wheel3: 0)!
-print(cgEvent.getIntegerValueField(.scrollWheelEventDeltaAxis1))
-print(cgEvent.getIntegerValueField(.scrollWheelEventPointDeltaAxis1))
-print(cgEvent.getIntegerValueField(.scrollWheelEventFixedPtDeltaAxis1))
